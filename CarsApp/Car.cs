f48dcbfe29cb1ec1127fe663f454d3acb6cb4a2d
Daniil <66083382+daniil2231@users.noreply.github.com>
2023-01-10 13:27:42 +0100

    Cars app
    A C# windows forms app where you can accelerate up to a set top speed and brake with a selected car

---
+++ CarsApp/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week14_Practice
+{
+    public class Car
+    {
+        private string model;
+        private int topSpeed;
+        private int speed;
+
+        public void SetModel(string model)
+        {
+            this.model = model;
+        }
+
+        public string GetModel()
+        {
+            return this.model;
+        }
+
+        public void SetTopSpeed(int topSpeed)
+        {
+            this.topSpeed = topSpeed;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            this.speed = speed;
+        }
+
+        public int GetSpeed()
+        {
+            return this.speed;
+        }
+
+        public void Accelerate(int increaseBy)
+        {
+            if (this.speed + increaseBy > this.topSpeed)
+            {
+                this.speed = this.topSpeed;
+            }
+            else
+            {
+                this.speed = this.speed + increaseBy;
+            }
+        }
+
+        public void Break()
+        {
+            this.speed = 0;
+        }
+
+        public string GetInfo()
+        {
+            return $"Car {this.model}: {this.speed}/{this.topSpeed}";
+        }
+    }
+}
